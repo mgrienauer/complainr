9cbb18d10de294855810fec8601f1582f001ac17
Michael Grienauer <mgrienauer@gmail.com>
2019-12-26 12:59:31 -0800

    Set up conditional navbar displays

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,34 +1,90 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { logoutUser } from "../../actions/authActions";
 
-export default class Navbar extends Component {
-    render() {
-        return (
-            <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
-                <div className="container">
-                <Link className="navbar-brand" to="/">Complainr</Link>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#mobile-nav">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
+class Navbar extends Component {
+  onLogoutClick = event => {
+    event.preventDefault();
+    this.props.logoutUser();
+  };
 
-                <div className="collapse navbar-collapse" id="mobile-nav">
-                    <ul className="navbar-nav mr-auto">
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/profiles">Users</Link>
-                    </li>
-                    </ul>
+  render() {
+    const { isAuthenticated, user } = this.props.auth;
 
-                    <ul className="navbar-nav ml-auto">
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/register">Sign Up</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/login">Login</Link>
-                    </li>
-                    </ul>
-                </div>
-                </div>
-            </nav>
-        )
-    }
+    const authLinks = (
+      <ul className="navbar-nav ml-auto">
+        <li className="nav-item">
+          <a href="#" onClick={this.onLogoutClick}>
+              <img 
+                src={user.avatar} 
+                alt={user.name} 
+                title="Must have gravatar linked to email for profile img"
+                style={{ 'width': '25px', marginRight: '5px' }}
+            />
+            Logout
+          </a>
+        </li>
+      </ul>
+    );
+
+    const guestLinks = (
+      <ul className="navbar-nav ml-auto">
+        <li className="nav-item">
+          <Link className="nav-link" to="/register">
+            Sign Up
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/login">
+            Login
+          </Link>
+        </li>
+      </ul>
+    );
+
+    return (
+      <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
+        <div className="container">
+          <Link className="navbar-brand" to="/">
+            Complainr
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#mobile-nav"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse" id="mobile-nav">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item">
+                <Link className="nav-link" to="/profiles">
+                  Users
+                </Link>
+              </li>
+            </ul>
+            {isAuthenticated ? authLinks : guestLinks}
+          </div>
+        </div>
+
+      </nav>
+    );
+  }
 }
+
+//define what prop types should be required by Navbar component
+Navbar.propTypes = {
+  logoutUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired
+};
+
+//get auth state from redux store
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(mapStateToProps, { logoutUser })(Navbar);
