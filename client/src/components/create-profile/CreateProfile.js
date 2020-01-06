77e9ae87552a81649736d98e6f0d24805f159606
Michael Grienauer <mgrienauer@gmail.com>
2020-01-06 08:59:39 -0800

    Add main info fields to CreateProfile

--- client/src/components/create-profile/CreateProfile.js
+++ client/src/components/create-profile/CreateProfile.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 import TextFieldGroup from '../common/TextFieldGroup'
+import TextAreaFieldGroup from '../common/TextAreaFieldGroup'
+import InputGroup from '../common/InputGroup'
+import SelectListGroup from '../common/SelectListGroup'
+
+
 
 class CreateProfile extends Component {
     state = {
@@ -32,7 +37,6 @@ class CreateProfile extends Component {
 
         //selection of status options
         const options = [
-           { label: 'Set Current Status', value: 0 },
            { label: 'Whiner', value: 'Whiner' },
            { label: 'Weenie', value: 'Weenie' },
            { label: 'Griper', value: 'Griper' },
@@ -42,29 +46,62 @@ class CreateProfile extends Component {
         ]
 
         return (
-            <div className="create-profile">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-8 m-auto">
-                            <h1 className="display-4 text-center">Create your profile</h1>
-                            <p className="lead text-center">
-                                Give us your infos plz
-                            </p>
-                            <small className="d-block pb-3">* = required</small>
-                        </div>
-                    </div>
+          <div className="create-profile">
+            <div className="container">
+              <div className="row">
+                <div className="col-md-8 m-auto">
+                  <h1 className="display-4 text-center">Create your profile</h1>
+                  <p className="lead text-center">Give us your sob story</p>
+                  <small className="d-block pb-3">* = required</small>
                 </div>
-                <form onSubmit={this.onSubmit}>
-                    <TextFieldGroup
-                        placeholder="* Profile Handle"
-                        name="handle"
-                        onChange={this.onChange}
-                        error={errors.handle}
-                        info="This is a display name that other users will see you by"
-                    />
-                </form>
+              </div>
             </div>
-        )
+            <form onSubmit={this.onSubmit}>
+              <TextFieldGroup
+                placeholder="* Profile Handle"
+                name="handle"
+                onChange={this.onChange}
+                error={errors.handle}
+                info="This is a display name that other users will see you by"
+              />
+
+              <SelectListGroup
+                placeholder="Status"
+                name="status"
+                value={this.state.status}
+                error={errors.select}
+                onChange={this.onChange}
+                options={options}
+                info="Select your current status you big baby"
+              />
+
+              <TextFieldGroup
+                placeholder="Location"
+                name="location"
+                onChange={this.onChange}
+                error={errors.location}
+                info="Where is your fortress of boo-hoo-itude?"
+              />
+
+              <TextFieldGroup
+                placeholder="* Main complaints"
+                name="complaints"
+                onChange={this.onChange}
+                error={errors.complaints}
+                info="Please enter your main complaints separated by commans (example: 'Work, boyfriend, bunyons'"
+              />
+
+              <TextAreaFieldGroup
+                placeholder="* Short bio"
+                name="bio"
+                onChange={this.onChange}
+                error={errors.bio}
+                info="Please give us your sob story. Keep it brief..."
+              />
+
+            </form>
+          </div>
+        );
     }
 }
 
