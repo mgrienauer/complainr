6bb76ee37c327ed96cba4108092f49b147c606b3
Michael Grienauer <mgrienauer@gmail.com>
2020-05-14 12:21:01 -0700

    add back to feed button

--- client/src/components/posts/PostsByUser.js
+++ client/src/components/posts/PostsByUser.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import Posts from "./Posts";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+
 import PropTypes from "prop-types";
 import { getProfileByHandle } from "../../actions/profileActions";
 
@@ -14,6 +16,14 @@ class PostsByUser extends Component {
 		let handle = this.props.match.params.handle;
 		return (
 			<>
+				<div className="row">
+					<div className="col-md-6">
+						<Link to="/feed" className="btn btn-light mb-3 float-left">
+							Back to Feed
+						</Link>
+					</div>
+					<div className="cold-md-6" />
+				</div>
 				<div className="row text-center mb-2">
 					<div className="col-12 d-flex justify-content-center">
 						<h2>Posts by {handle}</h2>
