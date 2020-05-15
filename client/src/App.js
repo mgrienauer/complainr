0e0789128367a1d9c55a6badc8b77bea1f21421b
Michael Grienauer <mgrienauer@gmail.com>
2020-05-15 14:39:36 -0700

    Init compile of sass

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./App.css";
-import "bootswatch/dist/flatly/bootstrap.min.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store";
